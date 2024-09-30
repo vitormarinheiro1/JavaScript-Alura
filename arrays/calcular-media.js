d90492e493952243b22e146085df0c2873d098db
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-09-30 16:46:36 -0300

    docs: adição de comentário sobre exemplo de array

--- arrays/calcular-media.js
+++ arrays/calcular-media.js
@@ -1,6 +1,9 @@
+// exemplo de array -> const lista = [10, "oi", true, [1, 2], { a: b }];
+
 const notas = [10, 6.5, 8, 7.5];
 
 const calcularMedia = () => {
-  return (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length};
+  return (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+};
 
 console.log(calcularMedia());
