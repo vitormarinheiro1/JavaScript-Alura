196031109b26feb1c7288a41068fb4e3f4da1872
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-09-30 16:36:53 -0300

    fix: alterando o numero 4 para notas.length

--- arrays/calcular-media.js
+++ arrays/calcular-media.js
@@ -1,7 +1,6 @@
 const notas = [10, 6.5, 8, 7.5];
 
 const calcularMedia = () => {
-  return (notas[0] + notas[1] + notas[2] + notas[3]) / 4;
-};
+  return (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length};
 
 console.log(calcularMedia());
