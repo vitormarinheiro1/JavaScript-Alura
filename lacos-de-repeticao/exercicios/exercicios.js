5260895c0e0138d24ae3351680d714ec5d965b9a
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-10-03 16:13:22 -0300

    feat: add exercicios

---
+++ lacos-de-repeticao/exercicios/exercicios.js
@@ -0,0 +1,13 @@
+// 1. Escreva um código que utilize o loop for/of para iterar e imprimir cada elemento de um array.
+
+// 2. Crie uma função que receba uma array e imprima no console o número do índice e o elemento.
+
+// 3. Crie uma função que receba uma array de números inteiros e retorne a soma dos elementos.
+
+// 4. Crie uma função que receba uma array de números inteiros e retorne o menor valor e o maior valor da array, no
+// seguinte formato: 'o menor número é X e o maior número é Y'.
+
+// 5. Crie um programa que utilize um laço for para percorrer uma array const numeros = [3, 8, 12, 5, 6, 10, 7, 2, 9, 14]
+// e exibir no console apenas os números pares contidos nesse array.
+
+// 6. Crie um programa que calcule a média dos números presentes em um array utilizando um loop for.
